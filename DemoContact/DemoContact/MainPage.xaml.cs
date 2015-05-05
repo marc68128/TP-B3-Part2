0eaee2b311e30b02f1c0e4e5125dad3402a37583
Marc Unterseh <148278@supinfo.com>
2015-05-05 14:55:59 +0200

    demo Contact

---
+++ DemoContact/DemoContact/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using DemoContact.Resources;
+using Microsoft.Phone.UserData;
+
+namespace DemoContact
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Contacts cons = new Contacts();
+            cons.SearchCompleted += ConsOnSearchCompleted;
+            cons.SearchAsync("Marc", FilterKind.DisplayName, "Contact Test");
+        }
+
+        private void ConsOnSearchCompleted(object sender, ContactsSearchEventArgs contactsSearchEventArgs)
+        {
+            var count = contactsSearchEventArgs.Results.Count();
+            Contact c = contactsSearchEventArgs.Results.First();
+        }
+    }
+}
